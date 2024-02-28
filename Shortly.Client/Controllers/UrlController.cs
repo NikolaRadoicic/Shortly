c8b499558303290256ed7da71255f0b3d0acfbe3
Nikola Radoičić <nikola.radoicic00@gmail.com>
2024-02-28 01:51:11 +0100

    Added Controllers, Model, View

---
+++ Shortly.Client/Controllers/UrlController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shortly.Client.Data.Models;
+
+namespace Shortly.Client.Controllers
+{
+    public class UrlController : Controller
+    {
+        public IActionResult Index()
+        {
+            //Data is from DataBase
+            {
+                ViewData["ShortenedUrl"] = "This is just a short url";
+                ViewData["AllUrls"] = new List<string>();
+                { }
+
+                return View();
+            }
+
+
+
+
+        }
+    }
+}
