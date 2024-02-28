0adf2ad91df7cf60cb827a1f41792fe6664b104d
Nikola Radoičić <nikola.radoicic00@gmail.com>
2024-02-28 17:31:41 +0100

    Designed home page,  navbar...

--- Shortly.Client/Controllers/HomeController.cs
+++ Shortly.Client/Controllers/HomeController.cs
@@ -17,10 +17,7 @@ namespace Shortly.Client.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+       
 
        
     }
--- Shortly.Client/Controllers/UrlController.cs
+++ Shortly.Client/Controllers/UrlController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Shortly.Client.Data.Models;
+using System.Collections.Generic; // Added for List<T>
 
 namespace Shortly.Client.Controllers
 {
@@ -7,18 +8,44 @@ namespace Shortly.Client.Controllers
     {
         public IActionResult Index()
         {
-            //Data is from DataBase
+            // Fake db data
+            var allUrls = new List<Url>()
             {
-                ViewData["ShortenedUrl"] = "This is just a short url";
-                ViewData["AllUrls"] = new List<string>();
-                { }
+                new Url()
+                {
+                    Id = 1,
+                    OriginalLink = "https://link1.com",
+                    ShortLink = "sh1",
+                    NumberOfClicks = 1,
+                    UserId = 1,
+                },
 
-                return View();
-            }
+                new Url()
+                {
+                    Id = 2,
+                    OriginalLink = "https://link2.com", // Corrected link
+                    ShortLink = "sh2",
+                    NumberOfClicks = 2,
+                    UserId = 2,
+                },
 
+                new Url()
+                {
+                    Id = 3,
+                    OriginalLink = "https://link3.com",
+                    ShortLink = "sh3",
+                    NumberOfClicks = 3,
+                    UserId = 3,
+                } 
+            };
 
+            // Pass the data to the view
+            return View(allUrls);
+        }
 
-
+        public IActionResult Create()
+        {
+            return RedirectToAction("Index");
         }
     }
 }
